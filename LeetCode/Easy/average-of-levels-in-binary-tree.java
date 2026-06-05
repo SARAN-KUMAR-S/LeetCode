4edb473b06cbe82cd3a07180bc26ca5f4b6ddf4f
SRN <152311778+SARAN-KUMAR-S@users.noreply.github.com>
2026-06-05 11:39:42 +0530

    Solved: Average of Levels in Binary Tree in Java

--- LeetCode/Easy/average-of-levels-in-binary-tree.java
+++ LeetCode/Easy/average-of-levels-in-binary-tree.java
@@ -1,38 +1,12 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
  *         this.left = left;
  *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public List<Double> averageOfLevels(TreeNode root) {
-        ArrayList<Double> ls = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            int n = q.size();
-            double ans =0;
-            for(int i = 0 ; i<n ; i++){
-                TreeNode r = q.poll();
-                ans+=r.val;
-                if(r.left!=null){
-                    q.add(r.left);
-                }
-                if(r.right!=null){
-                    q.add(r.right);
-                }
-            }
-            ls.add(ans/2);
-        }
-        return ls;
-    }
-}
+ *         this.val = val;
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *     TreeNode() {}
+ *     TreeNode right;
+ *     TreeNode left;
+ *     int val;
+ * public class TreeNode {
+ * Definition for a binary tree node.
+/**
